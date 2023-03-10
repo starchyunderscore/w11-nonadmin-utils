446f0984013aa18504edb02cf92a79615077990b
starchyunderscore <starchy_underscore@gmx.com>
2023-03-09 23:27:25 -0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,6 +3,7 @@ Write-Host "WARNING: THIS SCRIPT MESSES WITH THE REGISTRY. CREATE A RESTORE POIN
 $ContinueScript = $Host.UI.PromptForChoice("Are you sure you want to continue?", "(Default No)", @("&Yes", "&No"), 1)
 if ($ContinueScript -eq 1) {
 	Write-Host "User quit" -ForegroundColor Red
+	Exit
 }
 # starting text
 Write-Host "Starting script."
