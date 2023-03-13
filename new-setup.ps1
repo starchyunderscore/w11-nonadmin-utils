7fbb7ed85f1b2ef5486ebd383b383ac65b753f14
starchyunderscore <starchy_underscore@gmx.com>
2023-03-13 12:33:09 -0400

    Update new-setup.ps1

--- new-setup.ps1
+++ new-setup.ps1
@@ -284,7 +284,128 @@ public class Wallpaper
             }
           }
           2 { # PowerToys
-            
+            $InstallPowertoys = $Host.UI.PromptForChoice("Install Microsoft PowerToys?", "", @("&Cancel", "&Continue"), 0)
+            if ($InstallPowertoys -eq 1) {
+              $installLocation = "$env:LocalAppData\Programs\PowerToys"
+
+              $tempDir = Join-Path ([System.IO.Path]::GetTempPath()) (New-Guid)
+              New-Item -ItemType Directory -Path $tempDir -Force | Out-Null
+              Push-Location $tempDir
+
+              $latestPowerToys = Invoke-WebRequest "https://api.github.com/repos/microsoft/PowerToys/releases/latest" | ConvertFrom-Json
+              $latestVersion = $latestPowerToys.tag_name.Substring(1)
+              $isInstalled = Test-Path "$installLocation\PowerToys.exe"
+              if ($isInstalled) {
+                $currentVersion = (Get-Item "$installLocation\PowerToys.exe").VersionInfo.FileVersion
+                $currentVersion = $currentVersion.Substring(0, $currentVersion.LastIndexOf("."))
+              }
+
+              Write-Output "Latest: $latestVersion"
+              if (!$isInstalled) {
+                Write-Output "Current: Not installed"
+              }
+              else {
+                Write-Output "Current: $currentVersion"
+              }
+
+              if ($isInstalled -and ($latestVersion -le $currentVersion)) {
+                Write-Output "Already up to date"
+                Pop-Location
+                Remove-Item $tempDir -Force -Recurse
+                Read-Host "Finished! Press enter to exit"
+                exit
+              }
+
+              $latestPowerToys.assets | ForEach-Object {
+                $asset = $_
+                if ($asset.name -match "x64.exe$") {
+                  $assetUrl = $asset.browser_download_url
+                  $assetName = $asset.name
+                }
+              }
+
+              Write-Output "Downloading $assetName"
+              Start-BitsTransfer $assetUrl "$assetName" # Start-BitsTransfer instead of Invoke-WebRequest here to get a fancy progress bar (also BitsTransfer feels faster, but idk if this is true)
+              $powertoysInstaller = "$tempDir\$assetName"
+
+              $latestWix = Invoke-WebRequest "https://api.github.com/repos/wixtoolset/wix3/releases/latest" | ConvertFrom-Json
+              $latestWix.assets | ForEach-Object {
+                $asset = $_
+                if ($asset.name -match "binaries.zip$") {
+                  $assetUrl = $asset.browser_download_url
+                  $assetName = $asset.name
+                }
+              }
+
+              Write-Output "Downloading $assetName"
+              Start-BitsTransfer $assetUrl "$assetName"
+              $wixDir = "$tempDir\wix"
+              Expand-Archive "$tempDir\$assetName" -DestinationPath $wixDir
+
+              Write-Output "Extracting installer .exe"
+              $extractedInstaller = "$tempDir\extractedInstaller"
+              & "$wixDir\dark.exe" -x $extractedInstaller $powertoysInstaller | Out-Null
+
+              $msi = Get-ChildItem $extractedInstaller\AttachedContainer\*.msi
+              $extractedMsi = "$tempDir\extractedMsi"
+              Write-Output "Extracting installer .msi"
+              Start-Process -FilePath msiexec.exe -ArgumentList @( "/a", "$msi", "/qn", "TARGETDIR=$extractedMsi" ) -Wait
+
+              Write-Output "Stopping old instance (if running)"
+              Stop-Process -Name PowerToys -Force -ErrorAction SilentlyContinue
+              Start-Sleep 5 # To make sure the old instance is stopped
+
+              Write-Output "Installing new version"
+              Remove-Item "$installLocation" -Recurse -Force -ErrorAction SilentlyContinue
+              Copy-Item "$extractedMsi\PowerToys" -Destination $installLocation -Recurse
+
+              Write-Output "Creating hardlinks for runtimes"
+              # Code copy/pasted from https://github.com/ScoopInstaller/Extras/blob/0999a7377dd102c0f287ec191eed4866eb075562/bucket/powertoys.json#L24-L40
+              foreach ($f in @('Settings', 'modules\FileLocksmith', 'modules\Hosts', 'modules\MeasureTool', 'modules\PowerRename')) {
+                Get-ChildItem -Path "$installLocation\dll\WinAppSDK\" | ForEach-Object {
+                  New-Item -ItemType HardLink -Path "$installLocation\$f\$($_.Name)" -Value $_.FullName | Out-Null
+                }
+              }
+              foreach ($f in @('Settings', 'modules\Awake', 'modules\ColorPicker', 'modules\FancyZones', 
+                  'modules\FileExplorerPreview', 'modules\FileLocksmith', 'modules\Hosts', 'modules\ImageResizer', 
+                  'modules\launcher', 'modules\MeasureTool', 'modules\PowerAccent', 'modules\PowerOCR')) {
+                Get-ChildItem -Path "$installLocation\dll\Interop" | ForEach-Object {
+                  New-Item -ItemType HardLink -Path "$installLocation\$f\$($_.Name)" -Value $_.FullName | Out-Null
+                }
+                Get-ChildItem -Path "$installLocation\dll\dotnet\" | ForEach-Object {
+                  New-Item -ItemType HardLink -Path "$installLocation\$f\$($_.Name)" -Value $_.FullName -ErrorAction SilentlyContinue | Out-Null
+                }
+              }
+
+              Write-Output "Starting new instance"
+              Start-Process "$installLocation\PowerToys.exe"
+
+              if (!$isInstalled) {
+                $WshShell = New-Object -ComObject WScript.Shell
+
+                $createShortcut = $Host.UI.PromptForChoice("Create shortcut?", "Create a start menu shortcut for PowerToys?", @("&Yes", "&No"), 0)
+                if ($createShortcut -eq 0) {
+                  $Shortcut = $WshShell.CreateShortcut("$env:AppData\Microsoft\Windows\Start Menu\Programs\PowerToys.lnk")
+                  $Shortcut.TargetPath = "$installLocation\PowerToys.exe"
+                  $Shortcut.Save()
+                }
+
+                $autostart = $Host.UI.PromptForChoice("Autostart?", "Start PowerToys automatically on login?", @("&Yes", "&No"), 0)
+                if ($autostart -eq 0) {
+                  $Shortcut = $WshShell.CreateShortcut("$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\PowerToys.lnk")
+                  $Shortcut.TargetPath = "$installLocation\PowerToys.exe"
+                  $Shortcut.Save()
+                }
+              }
+
+              Write-Output "Cleaning up"
+              Pop-Location
+              Remove-Item -Path $tempDir -Recurse -Force
+
+              Write-Host "`nFinished installing powertoys!" -ForegroundColor Green
+            } else {
+              Write-Host "`nCanceled" -ForegroundColor Magenta
+            }
           }
           3 { # Visual Studio Code
             $InstallVSC = $Host.UI.PromptForChoice("Install visual studio code?", "", @("&Cancel", "&Continue"), 0)
