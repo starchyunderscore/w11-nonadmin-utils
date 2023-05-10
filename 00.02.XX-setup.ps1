84ffcca6434e9c2017fa73ee0908f647cc6d1138
starchyunderscore <starchy_underscore@gmx.com>
2023-05-10 15:19:39 -0400

    Update 00.02.XX-setup.ps1

--- 00.02.XX-setup.ps1
+++ 00.02.XX-setup.ps1
@@ -1,1 +1,23 @@
 # REWRITE OF SCRIPT WITH GUI WILL GO HERE
+Add-Type -assembly System.Windows.Forms
+$main_form = New-Object System.Windows.Forms.Form
+
+# Set the title and size of the window:
+
+$main_form.Text = "w11 nonadmin utils GUI"
+
+$main_form.Width = 600
+
+$main_form.Height = 400
+
+$main_form.AutoSize = $true
+
+# ADD STUFF HERE
+
+
+
+# SHOW FORM
+
+$main_form.ShowDialog()
+
+exit 0
